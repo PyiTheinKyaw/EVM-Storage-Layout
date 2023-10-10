bb1fe018070065981e6a58c5375c00be0253c657
Justin Thein Kyaw <uutxeinkyaw@gmail.com>
2023-10-10 20:16:14 +0630

    Finished String Calculation
    Finished String calculation of storage location in Ethereum.
    Created new file called 'StringLocationStorage' and
    Modify some configuration file for easy to use some function in console.

---
+++ contracts/StringLayoutInStorage.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.0 <0.9.0;
+
+contract StringLayoutInStorage
+{
+    string private s_short_str1;    // Slot 0
+    string private s_short_str2;    // Slot 1
+    string private s_long_str;      // Slot 2
+
+    constructor ()
+    {
+        /*
+            if the data is at most 31 bytes long, 
+            the elements are stored in the higher-order bytes (left aligned) 
+            and the lowest-order byte stores the value length * 2.
+
+            s_short_str1 = 0x48454c4c4f00000000000000000000000000000000000000000000000000000a
+
+            HELLO is 0x48454c4c4f only take 5 bytes. LSB is 0xa which is 10. (Hint: 5 bytes of hex is 10 characters)
+            Summary: if value is not over 31 bytes, the lowest-order byte store length_in_bytes * 2.
+        */
+        s_short_str1 = "HELLO";
+        s_short_str2 = "I LOVE OU";
+
+
+        /*
+            For byte arrays that store data which is 32 or more bytes long, 
+            the main slot p stores length * 2 + 1 and the data is stored as usual in keccak256(p). 
+
+            s_short_str = 0x0000000000000000000000000000000000000000000000000000000000000041
+            Value of s_long_str is 32 bytes long. LSB is 0x41 which is 65 in hex.
+
+            Summary: if value is 32 or more than, the lowest-order byte store (length_in_bytes * 2) + 1
+                     The location of data is store in keccak256(p), 'p' is slot position.
+
+                     > keccka256(2) = soliditySha3(padLeft('0x2', 64)) = 0x405787fa12a823e0f2b7631cc41b3ba8828b3321ca811111fa75cd3aa3bb5ace
+                     > At keccak256(2) => 0x41414178644b793142733346684250305a456b394c49595571526a3979414141 
+                                          which is 'AAAxdKy1Bs3FhBP0ZEk9LIYUqRj9yAAA'.
+
+        */
+        s_long_str = "AAAxdKy1Bs3FhBP0ZEk9LIYUqRj9yAAA"; // 32 bytes long
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -11,4 +11,13 @@ extendEnvironment((hre) => {
 
   // hre.network.provider is an EIP1193-compatible provider.
   hre.web3 = new Web3(hre.network.provider);
+
+  // web3-eth module here.
+  hre.getStorageAt = hre.web3.eth.getStorageAt;
+  
+  //web3.utils module here.
+  hre.hex2num = hre.web3.utils.hexToNumber;
+  hre.hex2str = hre.web3.utils.hexToString;
+  hre.soliditySha3 = hre.web3.utils.soliditySha3;
+  hre.padLeft = hre.web3.utils.padLeft;
 });
